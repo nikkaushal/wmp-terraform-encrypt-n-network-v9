c7479de06244a007523639447959e5126e46e7eb
nikkaushal <nikkaushal@gmial.com>
2026-05-05 21:08:29 -0400

    network

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -16,23 +16,23 @@ resource "aws_subnet" "main" {
   
 }
 
-resource "aws_vpc_peering_connection" "main" {
-  vpc_id        = var.default_vpc_id
-  peer_vpc_id   = aws_vpc.main.id
-  auto_accept = true 
-  tags = {
-    Name = "default-to-${var.env}"
-  }
+# resource "aws_vpc_peering_connection" "main" {
+#   vpc_id        = var.default_vpc_id
+#   peer_vpc_id   = aws_vpc.main.id
+#   auto_accept = true 
+#   tags = {
+#     Name = "default-to-${var.env}"
+#   }
   
-}
+# }
 
-resource "aws_route" "default-rt-add-peering" {
-  route_table_id         = var.default_vpc_rt_id
-  destination_cidr_block = var.vpc_cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
-}
-resource "aws_route" "here-vpc-rt-add-peering" {
-  route_table_id         = aws_vpc.main.default_route_table_id
-  destination_cidr_block = var.default_vpc_cidr
-  vpc_peering_connection_id = aws_vpc_peering_connection.main.id
-}
+# resource "aws_route" "default-rt-add-peering" {
+#   route_table_id         = var.default_vpc_rt_id
+#   destination_cidr_block = var.vpc_cidr
+#   vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+# }
+# resource "aws_route" "here-vpc-rt-add-peering" {
+#   route_table_id         = aws_vpc.main.default_route_table_id
+#   destination_cidr_block = var.default_vpc_cidr
+#   vpc_peering_connection_id = aws_vpc_peering_connection.main.id
+# }
