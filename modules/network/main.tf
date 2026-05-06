376a760d183024ec33dddf85d6915d60754d1c2a
nikkaushal <nikkaushal@gmial.com>
2026-05-05 23:04:35 -0400

    network

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -42,12 +42,12 @@ resource "aws_route" "igw-route" {
 }
 # NAT Gateway EIP - one per igw subnet AZ
 resource "aws_eip" "ngw" {
-  for_each = local.igw_subnets
+  for_each = local.ngw_subnets
   domain   = "vpc"
 }
 # NAT Gateway - one per ngw subnet
 resource "aws_nat_gateway" "ngw" {
-  for_each      = local.igw_subnets
+  for_each      = local.ngw_subnets
   allocation_id = aws_eip.ngw[each.key].id
   subnet_id     = aws_subnet.main[each.key].id
 }
@@ -58,7 +58,7 @@ resource "aws_route" "ngw-route" {
   route_table_id         = aws_route_table.main[each.key].id
   destination_cidr_block = "0.0.0.0/0"
   # Look up NAT GW by matching AZ of the app subnet to AZ of the public subnet
-  nat_gateway_id         = aws_nat_gateway.ngw[local.az_to_ngw[each.value.az]].id
+  nat_gateway_id         =  aws_nat_gateway.main[each.key].id
 }
 # resource "aws_vpc_peering_connection" "main" {
 #   vpc_id        = var.default_vpc_id
