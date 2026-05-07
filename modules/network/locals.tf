8211fc035cd4af0d0c5a5d833d7b8d874b8fe3d8
nikkaushal <nikkaushal@gmial.com>
2026-05-06 23:59:36 -0400

    network

--- modules/network/locals.tf
+++ modules/network/locals.tf
@@ -1,16 +1,34 @@
 locals {
   # Filter subnets where igw = true
+#   igw_subnets = {
+#     for subnet_name, subnet_config in var.subnets :
+#     subnet_name => subnet_config
+#     if subnet_config.igw == true
+#   }
+
+#   # Filter subnets where ngw = true
+#   ngw_subnets = {
+#     for subnet_name, subnet_config in var.subnets :
+#     subnet_name => subnet_config
+#     if subnet_config.ngw == true
+#   }
   igw_subnets = {
     for subnet_name, subnet_config in var.subnets :
     subnet_name => subnet_config
     if subnet_config.igw == true
   }
 
-  # Filter subnets where ngw = true
   ngw_subnets = {
     for subnet_name, subnet_config in var.subnets :
     subnet_name => subnet_config
     if subnet_config.ngw == true
+  }
+
+  # Map AZ -> NAT Gateway key (public subnet name)
+  # e.g. "us-east-1a" -> "public-subnet1"
+  az_to_ngw = {
+    for subnet_name, subnet_config in local.igw_subnets :
+    subnet_config.az => subnet_name
   }
     public_subnets = {
     for subnet_name, subnet_config in var.subnets :
@@ -30,28 +48,3 @@ locals {
     if subnet_config.group == "db"
   }
 }
-
-
-
-# modules/network/locals.tf
-
-# locals {
-#   igw_subnets = {
-#     for subnet_name, subnet_config in var.subnets :
-#     subnet_name => subnet_config
-#     if subnet_config.igw == true
-#   }
-
-#   ngw_subnets = {
-#     for subnet_name, subnet_config in var.subnets :
-#     subnet_name => subnet_config
-#     if subnet_config.ngw == true
-#   }
-
-#   # Map AZ -> NAT Gateway key (public subnet name)
-#   # e.g. "us-east-1a" -> "public-subnet1"
-#   az_to_ngw = {
-#     for subnet_name, subnet_config in local.igw_subnets :
-#     subnet_config.az => subnet_name
-#   }
-# }
