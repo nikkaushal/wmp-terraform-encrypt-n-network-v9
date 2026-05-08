226a0c7ffc2ed40c2756704c4c9bf3a7a9ed92e1
nikkaushal <nikkaushal@gmial.com>
2026-05-07 20:27:22 -0400

    network

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -1,13 +1,13 @@
-# resource "null_resource" "kube-config" {
-#     depends_on = [aws_eks_node_group.main]
-#     triggers = {
-#         alltime = timestamp()
-#     }
-#     provisioner "local-exec" {
-#         command = "aws eks update-kubeconfig --name ${var.env}"
-#     }
+resource "null_resource" "kube-config" {
+    depends_on = [aws_eks_node_group.main]
+    triggers = {
+        alltime = timestamp()
+    }
+    provisioner "local-exec" {
+        command = "aws eks update-kubeconfig --name ${var.env}"
+    }
     
-# }
+}
 
 provider "helm" {
   kubernetes = {
