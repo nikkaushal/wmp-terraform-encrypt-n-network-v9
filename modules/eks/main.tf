02cab2ab4120b06d8d18d3fed6244d96b6308159
nikkaushal <nikkaushal@gmial.com>
2026-05-11 19:38:59 -0400

    added extrernal dns code block for Helm

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -22,6 +22,10 @@ resource "aws_eks_cluster" "main" {
   depends_on = [aws_iam_role_policy_attachment.cluster_AmazonEKSClusterPolicy]
 }
 
+resource "aws_eks_addon" "eks-pos-identity" {
+  cluster_name = aws_eks_cluster.main.name
+  addon_name   = " eks-pod-identity-agent"
+}
 resource "aws_vpc_security_group_ingress_rule" "add-https-to-bastion" {
   security_group_id = aws_eks_cluster.main.vpc_config[0].cluster_security_group_id
   cidr_ipv4         = "172.31.0.0/16"
