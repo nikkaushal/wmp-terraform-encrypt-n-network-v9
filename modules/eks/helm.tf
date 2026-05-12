92b91655409a91f1f67b87a2c1ba5f39fc7a715f
nikkaushal <nikkaushal@gmial.com>
2026-05-11 21:17:39 -0400

    added extrernal dns code block for Helm

--- modules/eks/helm.tf
+++ modules/eks/helm.tf
@@ -89,6 +89,7 @@ resource "helm_release" "traefik" {
   name       = "traefik"
   repository = "https://traefik.github.io/charts"
   chart      = "traefik"
+  values     = [file("${path.module}/traefik.yml")]
 
 }
 
